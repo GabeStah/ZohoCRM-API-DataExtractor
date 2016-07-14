b4d19c3309e7f1b3e57e410204f9b2cbe87aaebf
Gabe Wyatt <gwyattkelsey@gmail.com>
2016-07-14 09:29:33 -0700

    Added basic `setup.py`.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ZohoCRM',
+    version='1.0.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Gabe Wyatt',
+    author_email='gabe@gabewyatt.com',
+    description='Extract Zoho CRM API data and upload to AWS S3.'
+)
